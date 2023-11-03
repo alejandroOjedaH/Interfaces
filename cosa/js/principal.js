f1446a49cd27e42f99425587a2969758b76e69d1
alejandro <limonmagico1@gmail.com>
2023-11-03 19:52:38 +0100

    La nevecita de mierda

--- cosa/js/principal.js
+++ cosa/js/principal.js
@@ -1,3 +1,29 @@
+window.onload = () =>{
+    establecerSesion();
+    establecerTiempo();
+}
+
+function establecerTiempo(){
+    const reloj = document.getElementById("eta");
+}
+function establecerSesion(){
+    if(sessionStorage.getItem("supervivientes") === null){
+        sessionStorage.setItem("supervivientes","90");
+    }
+    if(sessionStorage.getItem("energia") === null){
+        sessionStorage.setItem("energia","90");
+    }
+    if(sessionStorage.getItem("integridad") === null){
+        sessionStorage.setItem("integridad",["90"]);
+    }
+    if(sessionStorage.getItem("misiles") === null){
+        sessionStorage.setItem("misiles","10");
+    }
+    if(sessionStorage.getItem("tripilacion") === null){
+        sessionStorage.setItem("tripulacion",[]);
+    }
+}
+
 
 function mostrarEscudoInterface() {
     ocultarTodasLasInterfaces();
