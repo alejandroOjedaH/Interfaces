04b730be7b1da2e09c368a5c593a21a5c8caae28
alejandro <limonmagico1@gmail.com>
2023-11-07 19:32:01 +0100

    esto

---
+++ cartajuego/javascript.js
@@ -0,0 +1,30 @@
+window.onload = () =>{
+    const tablero = document.getElementById("juego");
+    const cartas = [];
+
+    function randomizador{
+        let string = '';
+        let c0 = 0 ,c1 = 0, c2=0, c3=0, c4=0, c5=0, c=0;
+
+        while(c<12){
+            const aleatorio =Math.random*6;
+            if(aleatorio == 0 && c0 <2){
+
+            }else if(aleatorio == 1 && c1 <2){
+                
+            }else if(aleatorio == 2 && c2 <2){
+                
+            }else if(aleatorio == 3 && c3 <2){
+                
+            }else if(aleatorio == 4 && c4 <2){
+                
+            }else if(aleatorio == 5 && c5 <2){
+                
+            }
+            c++;
+        }
+
+
+
+    }
+}
