392d187c93dffbe74e1b39dc77247f45be10597d
alejandro <limonmagico1@gmail.com>
2023-11-28 17:09:23 +0100

    juego de cartas completo

--- cartajuego/javascript.js
+++ cartajuego/javascript.js
@@ -1,7 +1,12 @@
 window.onload = () =>{
+    cartaAnterior=null;
+    clicks=parseInt(0);
     const tablero = document.getElementById("juego");
+    let puntuacion = document.getElementById("puntuacion");
+    puntuacion.innerHTML = clicks;
     
     randomizador();
+    setearEventos();
 
     function randomizador(){
         let cadena = "";
@@ -10,26 +15,69 @@ window.onload = () =>{
         while(c0 <2 || c1 <2 || c2<2 || c3<2 || c4<2 || c5<2 || c<2){
             let aleatorio = parseInt(Math.random()*6);
             if(aleatorio == 0 && c0 <2){
-                cadena += "<div class=\"carta lilianaid\"><div class=\"trasera\"></div></div>";
+                cadena += "<div class=\"carta\"><div class=\"liliana frontal\"></div><div class=\"trasera\"></div></div>";
                 c0++;
             }else if(aleatorio == 1 && c1 <2){
-                cadena += "<div class=\"carta emrakulid\"><div class=\"trasera\"></div></div>";
+                cadena += "<div class=\"carta\"><div class=\"emrakul frontal\"></div><div class=\"trasera\"></div></div>";
                 c1++;
             }else if(aleatorio == 2 && c2 <2){
-                cadena += "<div class=\"carta decimodoctorid\"><div class=\"trasera\"></div></div>";
+                cadena += "<div class=\"carta\"><div class=\"decimodoctor frontal\"></div><div class=\"trasera\"></div></div>";
                 c2++;
             }else if(aleatorio == 3 && c3 <2){
-                cadena += "<div class=\"carta counterspellid\"><div class=\"trasera\"></div></div>";
+                cadena += "<div class=\"carta\"><div class=\"counterspell frontal\"></div><div class=\"trasera\"></div></div>";
                 c3++;
             }else if(aleatorio == 4 && c4 <2){
-                cadena += "<div class=\"carta montananevada\"><div class=\"trasera\"></div></div>";
+                cadena += "<div class=\"carta\"><div class=\"montananevada frontal\"></div><div class=\"trasera\"></div></div>";
                 c4++;
             }else if(aleatorio == 5 && c5 <2){
-                cadena += "<div class=\"carta iraid\"><div class=\"trasera\"></div></div>";
+                cadena += "<div class=\"carta\"><div class=\"ira frontal\"></div><div class=\"trasera\"></div></div>";
                 c5++;
             }
             c++;
         }  
         tablero.innerHTML = cadena;
+    }
+    function setearEventos(){
+        let cartas =document.getElementsByClassName("carta");
+        for (let i = 0; i < cartas.length; i++) {
+            const carta = cartas[i];
+            
+            carta.onclick= seleccionarCarta;
+        }
+    }
+    
+    function seleccionarCarta(evento){
+        let cartaActual = evento.target;
+        cartaActual.classList.add("seleccionada");
+        clicks++;
+        puntuacion.innerHTML = clicks;
+        if(cartaAnterior === null){
+            cartaAnterior = cartaActual;
+        }else{
+            let tipoCartaActual = cartaActual.getElementsByClassName("frontal")[0].classList[0];
+            let tipoCartaAnterior = cartaAnterior.getElementsByClassName("frontal")[0].classList[0];
+            let cartas =document.getElementsByClassName("carta");
+            for (let index = 0; index < cartas.length; index++) {
+                const carta = cartas[index];
+                carta.classList.add("notocar");
+            }
+            if(tipoCartaActual !== tipoCartaAnterior){
+                setTimeout(() => {
+                    cartaAnterior.classList.remove("seleccionada");
+                    cartaActual.classList.remove("seleccionada");
+                    cartaAnterior = null;
+                    for (let index = 0; index < cartas.length; index++) {
+                        const carta = cartas[index];
+                        carta.classList.remove("notocar");
+                    }
+                }, 1000);
+            }else{
+                cartaAnterior = null; 
+                for (let index = 0; index < cartas.length; index++) {
+                    const carta = cartas[index];
+                    carta.classList.remove("notocar");
+                }
+            } 
+        }
     }
 }
